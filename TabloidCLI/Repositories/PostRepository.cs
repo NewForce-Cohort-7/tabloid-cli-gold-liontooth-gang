8109edef7fb8d831076cb912e0d64cccbdb3012c
Timothy Clark <TimDMClark@gmail.com>
2023-06-16 15:24:15 -0400

    Fixed main menu, commented code in PostRepository, added Add Blog, List Blogs, Remove Blog, and Edit Blog.

--- TabloidCLI/Repositories/PostRepository.cs
+++ TabloidCLI/Repositories/PostRepository.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using Microsoft.Data.SqlClient;
 using TabloidCLI.Models;
+using TabloidCLI.UserInterfaceManagers;
 
 namespace TabloidCLI.Repositories
 {
@@ -60,12 +61,12 @@ namespace TabloidCLI.Repositories
                                 LastName = reader.GetString(reader.GetOrdinal("LastName")),
                                 Bio = reader.GetString(reader.GetOrdinal("Bio")),
                             },
-                            Blog = new Blog()
-                            {
-                                Id = reader.GetInt32(reader.GetOrdinal("BlogId")),
-                                Title = reader.GetString(reader.GetOrdinal("BlogTitle")),
-                                Url = reader.GetString(reader.GetOrdinal("BlogUrl")),
-                            }
+                            //Blog = new BlogRepository()
+                            //{
+                            //    Id = reader.GetInt32(reader.GetOrdinal("BlogId")),
+                            //    Title = reader.GetString(reader.GetOrdinal("BlogTitle")),
+                            //    Url = reader.GetString(reader.GetOrdinal("BlogUrl")),
+                            //}
                         };
                         posts.Add(post);
                     }
@@ -93,3 +94,115 @@ namespace TabloidCLI.Repositories
         }
     }
 }
+
+// DAVID: This code was written by me when I didn't realize main menu was already done and I was building a bas
+// Feel free to use any of it that you see fit
+// -Tim
+
+//namespace TabloidCLI
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+
+
+//            IUserInterfaceManager ui = new MainMenuManager();
+//            while (ui != null)
+//            {
+//                PrintMainMenu();
+//                string selection = GetMenuSelection();
+
+//                switch (selection)
+//                {
+//                    case ("List Posts"):
+//                        Console.Write("Press any key to continue");
+//                        Console.ReadKey();
+//                        break;
+
+//                    case ("Add Post"):
+//                        Console.Write("Press any key to continue");
+//                        Console.ReadKey();
+//                        break;
+
+//                    case ("Edit Post"):
+//                        Console.Write("Press any key to continue");
+//                        Console.ReadKey();
+//                        break;
+
+//                    case ("Remove Post"):
+//                        Console.Write("Press any key to continue");
+//                        Console.ReadKey();
+//                        break;
+
+//                    case ("Note Management"):
+//                        Console.Write("Press any key to continue");
+//                        Console.ReadKey();
+//                        break;
+
+//                    case ("Return to Main Menu"):
+//                        Console.Write("Press any key to continue");
+//                        Console.ReadKey();
+//                        break;
+
+//                    default:
+//                        Console.WriteLine("Invalid selection. Please try again.");
+//                        break;
+//                }
+//            }
+//        }
+
+//        static void PrintMainMenu()
+//        {
+//            Console.WriteLine("Main Menu:");
+//            Console.WriteLine("1) My Journal Management");
+//            Console.WriteLine("2) Blog Management");
+//            Console.WriteLine("3) Author Management");
+//            Console.WriteLine("4) Post Management");
+//            Console.WriteLine("5) Tag Management");
+//            Console.WriteLine("6) Search by Tag");
+//            Console.WriteLine("7) Exit");
+//            Console.WriteLine();
+//        }
+
+//        static string GetMenuSelection()
+//        {
+//            Console.Clear();
+
+//            Console.Write("Menu Selection: ");
+//            Console.WriteLine("");
+//            List<string> options = new List<string>()
+//            {
+//                 "List Posts",
+//                 "Add Post",
+//                 "Edit Post",
+//                 "Remove Post",
+//                 "Note Management",
+//                 "Return to Main Menu",
+//            };
+
+//            for (int i = 0; i < options.Count; i++)
+//            {
+//                Console.WriteLine($"{i + 1}. {options[i]}");
+//            }
+
+//            while (true)
+//            {
+//                try
+//                {
+//                    Console.WriteLine();
+//                    Console.Write("Select an option > ");
+
+//                    string input = Console.ReadLine();
+//                    int index = int.Parse(input) - 1;
+//                    return options[index];
+//                }
+//                catch (Exception)
+//                {
+
+//                    continue;
+//                }
+//            }
+//        }
+//    }
+//}
